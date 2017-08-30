62fe34242485e594d7924f3178a48ba1c6d4f3dd
Bernard Strong <bstrong71@gmail.com>
2017-08-30 15:23:31 -0400

    add task and complete task functional

---
+++ migrations/20170830184441-create-todo.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      task: {
+        type: Sequelize.STRING
+      },
+      complete: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Todos');
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,41 @@ const models    = require("../models/index");
 
 const router    = express.Router();
 
-
+//find all Todos when hitting main page//
 router.get("/", function(req, res) {
-
-  res.render("index");
+  models.Todo.findAll()
+    .then(function(data) {
+      console.log("THIS IS TODO DATA", data);
+      res.render("index", {todo: data});
+    });
 });
 
-router.get("/complete/:task", function(req, res) {
-  todos[req.params.task - 1].complete = true;
-  res.redirect("/");
-})
-
+//creates and saves all in one step//
 router.post("/", function(req, res) {
-  let obj = {task: req.body.task, complete: false, id: todos.length + 1};
-  todos.push(obj);
-  res.redirect("/");
-})
+  models.Todo.create({
+    task: req.body.task,
+  })
+  .then(function(data) {
+    res.redirect("/");
+  });
+});
+
+//complete tast//
+router.get("/complete/:id", function(req, res) {
+  models.Todo.update({
+    name: req.body.name,
+    complete: true
+  }, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(function(data) {
+      res.redirect("/");
+    });
+});
+
+
 
 
 module.exports = router;
